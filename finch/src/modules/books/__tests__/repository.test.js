62025db1e1348e5ad6da4017b3e46fa0ef2f94a3
Anshul Sahni <anshul_sahni@live.com>
2021-05-30 21:17:09 +0530

    🩺test(books): added test for repository

---
+++ finch/src/modules/books/__tests__/repository.test.js
@@ -0,0 +1,38 @@
+import UserRepository from '../repository.js';
+
+describe('#BooksRepository', () => {
+  describe('##findByPublicIdOrFail', () => {
+    test('should return the object returned by model', async () => {
+      const _book = {
+        id: 'sampleId12',
+        tenant: {
+          name: 'Tenant Name',
+          email: 'tenant@gmail.com',
+        },
+        landlord: {
+          name: 'Landlord Name',
+          email: 'landlord@gmail.com',
+        },
+        receipts: [],
+      };
+
+      UserRepository.findOne = jest.fn();
+      UserRepository.findOne.mockReturnValueOnce(_book);
+
+      const result = await UserRepository
+        .findByPublicIdOrFail('sampleId12');
+
+      expect(result).toEqual(_book);
+    });
+
+    it('should throw an error when model returns null', () => {
+      UserRepository.findOne = jest.fn();
+      UserRepository.findOne.mockReturnValueOnce(null);
+
+      expect(async () => {
+        await UserRepository.findByPublicIdOrFail('anshul');
+      }).rejects.toThrow();
+    });
+
+  });
+});
