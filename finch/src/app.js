21a5febc4723a3d80d17a2b7bdfe704345058154
Anshul Sahni <anshul_sahni@live.com>
2021-06-27 14:30:35 +0530

    🍺feat(error): catchController to handle error thrown from controller

--- finch/src/app.js
+++ finch/src/app.js
@@ -5,6 +5,8 @@ import config from '../config/index.js';
 
 import ReceiptController from './modules/books/controller.js';
 
+import CatchController from './middlewares/catch-controller.js';
+
 import HttpServer from './http-server.js';
 import Database from './services/database.js';
 
@@ -22,9 +24,10 @@ const server = new HttpServer({
 
 const database = new Database(config.database);
 
-
 server.start();
 
 database.connect();
 
 new ReceiptController({ app: server.app }).initRoutes();
+
+new CatchController({ app: server.app }).init();
