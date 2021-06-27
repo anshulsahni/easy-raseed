008eb4b89235d7ce2678dc87080525517c679a9a
Anshul Sahni <anshul_sahni@live.com>
2021-06-27 17:30:56 +0530

    🍺feat(http): added list of constants of status codes

---
+++ finch/src/http/status-codes.js
@@ -0,0 +1,12 @@
+/* HTTP Code Constant */
+export const HTTP_RESOURCE_CREATED = 201;
+export const HTTP_OK = 200;
+
+export const BAD_REQUEST = 400;
+export const RESOURCE_NOT_FOUND = 404;
+export const UNAUTHORIZED = 401;
+export const FORBIDDEN = 403;
+
+
+// Server Errors
+export const INTERNAL_SERVER_ERROR = 500;
