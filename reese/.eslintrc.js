ab76ffedf04faba19597300b5ed998af0e267076
Anshul Sahni <anshul_sahni@live.com>
2021-07-17 17:01:54 +0530

    📦package: eslint added & versions of others corrected
    	New packages
    		- eslint
    		- eslint-config-kentcdodds
    	Versions Corrected
    		- css-loader
    		- html-webpackage-plugin

---
+++ reese/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['kentcdodds', 'kentcdodds/react'],
+  rules: {
+    'react/jsx-filename-extension': [
+      'error', {
+        'allow': 'as-needed',
+      },
+    ],
+  },
+};
